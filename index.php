b523005349080f8731b9de02e11653ddf33baf85
onk0611 <theos.pro@pm.me>
2022-08-23 14:40:57 +0200

    rm footer & add css

--- index.php
+++ index.php
@@ -2,3 +2,12 @@
 
 // Connexion à la base de données
 require './assets/db.php';
+
+// ajout du Header
+require './assets/conponents/header.php';
+
+// ajout du filtre
+require './assets/filter.php';
+
+// affichage des offres d'emploies
+require './assets/card.php';
